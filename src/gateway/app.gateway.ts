e6a0984f6e3def6b090fbad7057888bcf035b55d
thobui14ig <buithanhtho14ig@gmail.com>
2023-11-05 16:50:04 +0700

    ok

--- src/gateway/app.gateway.ts
+++ src/gateway/app.gateway.ts
@@ -186,7 +186,7 @@ export class AppGateway
   getPhoneNumber(inputString: string) {
     const phonePattern = /\d{10,11}/; // Biểu thức chính quy để tìm số điện thoại có độ dài từ 10 đến 11 chữ số
 
-    const phoneNumber = inputString.match(phonePattern);
+    const phoneNumber = inputString?.match(phonePattern);
 
     if (phoneNumber) {
       return phoneNumber[0];
